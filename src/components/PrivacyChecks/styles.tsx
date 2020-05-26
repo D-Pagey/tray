af045e6a6e1904e67df4f2291f2e159f2766b74d
Dan Page <dan.page91@gmail.com>
2020-05-26 02:32:23 +0100

    fix(privacy): add flexbox

--- src/components/PrivacyChecks/styles.tsx
+++ src/components/PrivacyChecks/styles.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Form = styled.form`
     padding: 1rem;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const InputWrapper = styled.div`
