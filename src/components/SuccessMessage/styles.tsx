ff047076614c2dc96269355acd4a273f161e5924
Dan Page <dan.page91@gmail.com>
2020-05-26 02:24:45 +0100

    fix(success message): add basic styles to success message

--- src/components/SuccessMessage/styles.tsx
+++ src/components/SuccessMessage/styles.tsx
@@ -2,10 +2,17 @@ import styled from 'styled-components';
 import { colour } from '../../tokens';
 
 export const Wrapper = styled.div`
+    border: 1px solid ${colour.black};
+    background-color: ${colour.white};
+    border-radius: 5px;
     display: flex;
+    justify-content: center;
+    margin: 2rem 0 0;
+    padding: 1rem 0;
 `;
 
 export const Text = styled.p`
     color: ${colour.green};
     text-align: center;
+    width: 400px;
 `;
